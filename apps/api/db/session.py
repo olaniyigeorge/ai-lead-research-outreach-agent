277fd0e754f59f065185ebff99484b370422a162
olaniyigeorge <olaniyigeorge77@gmail.com>
2026-09-22 02:16:01 +0100

    Add SQLAlchemy engine and session factory

---
+++ apps/api/db/session.py
@@ -0,0 +1,31 @@
+from collections.abc import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from apps.api.config import get_settings
+
+_engine = None
+_SessionLocal = None
+
+
+def get_engine():
+    global _engine
+    if _engine is None:
+        _engine = create_engine(get_settings().database_url, pool_pre_ping=True)
+    return _engine
+
+
+def get_session_factory() -> sessionmaker:
+    global _SessionLocal
+    if _SessionLocal is None:
+        _SessionLocal = sessionmaker(bind=get_engine(), autoflush=False, autocommit=False)
+    return _SessionLocal
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = get_session_factory()()
+    try:
+        yield db
+    finally:
+        db.close()
